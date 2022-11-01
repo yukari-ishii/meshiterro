eb76eae3a40f135df5f3739cddbb8116591056b7
yukari-ishii <115003222+yukari-ishii@users.noreply.github.com>
2022-11-01 13:40:42 +0000

    [Finish] 4-20

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
+  get "/homes/about" => "homes#about", as: "about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
-  get 'homes/about' => 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
